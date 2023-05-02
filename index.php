1770787ef8ee6bc624860a8874c713e4590e47b1
alan2442 <alan.nogueira40@hotmail.com>
2023-05-02 18:50:06 -0300

    criei o topo com barra horizontal e criei uma barra lateral

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Information</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body id="" class="home">
+    
+    <div id="container"><!-- Início Container -->
+
+        <div id="topo"> <!-- Início Topo-->
+            
+            <h1 class="logo">News Information</h1>
+            
+            <ul id="navegacao"><!-- Início Barra de Navegação Horizontal -->
+
+                <li class="primeiro">
+                    <a href="index.php" id="home">Home</a>
+                </li>
+
+                <li>
+                    <a href="brasil.php" id="brasil">Brasil</a>
+                </li>
+
+                <li>
+                    <a href="internacional.php" id="internacional">internacional</a>
+                </li>
+
+                <li>
+                    <a href="economia.php" id="economia">economia</a>
+                </li>
+
+                <li>
+                    <a href="saude.php" id="saude">Saúde</a>
+                </li>
+
+                <li>
+                    <a href="ciencia.php" id="ciencia">Ciência</a>
+                </li>
+
+                <li>
+                    <a href="fotos.php" id="fotos">Fotos</a>
+                </li>
+
+            </ul> <!-- Fim Barra de Navegação Horizontal -->
+
+
+        </div> <!-- Fim Topo -->
+
+        <div id="conteudo"> <!-- Início Conteudo-->
+
+            <div id="primario"> <!-- Início Conteudo Primario-->
+                Conteudo Primario
+            </div> <!-- Fim Conteudo-->
+
+            <div id="secundario"> <!-- Início Conteudo Secundario-->
+                Conteudo Secundario
+            </div> <!-- Fim Conteudo Secundario-->
+
+            <div id="lateral"> <!-- Início Barra lateral-->
+
+                <div class="caixa"> <!-- Início Caixa-->
+
+                    <h2>Entrevista</h2>
+                    <ul>
+                        <li><a href="">José Almeida</a></li>
+                        <li><a href="">Felipe Costa</a></li>
+                        <li><a href="">Humberto de Alencar</a></li>
+                        <li><a href="">Maria das Neves</a></li>
+                        <li><a href="">Anderson Gomes</a></li>
+                        <li><a href="">Marta Ribeiro</a></li>
+                        <li><a href="">Airton Senna</a></li>
+                        <li><a href="">Tim Maia</a></li>
+                    </ul>
+                </div> <!-- Fim caixa-->
+
+            </div> <!-- Fim Barra lateral-->
+
+
+        </div> <!-- Fim Conteudo-->
+
+    </div> <!-- FIM Container -->
+
+</body>
+</html>
---
+++ saude.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Notícias</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="saude">
+    
+    <div id="container"><!-- Início Container -->
+
+        <div id="topo"> <!-- Início Topo-->
+            
+            <h1 class="logo">News Information</h1>
+            
+            <ul id="navegacao"><!-- Início Barra de Navegação Horizontal -->
+
+                <li class="primeiro">
+                    <a href="index.php" id="home">Home</a>
+                </li>
+
+                <li>
+                    <a href="brasil.php" id="brasil">Brasil</a>
+                </li>
+
+                <li>
+                    <a href="internacional.php" id="internacional">internacional</a>
+                </li>
+
+                <li>
+                    <a href="economia.php" id="economia">economia</a>
+                </li>
+
+                <li>
+                    <a href="saude.php" id="saude">Saúde</a>
+                </li>
+
+                <li>
+                    <a href="ciencia.php" id="ciencia">Ciência</a>
+                </li>
+
+                <li>
+                    <a href="fotos.php" id="fotos">Fotos</a>
+                </li>
+
+            </ul> <!-- Fim Barra de Navegação Horizontal -->
+
+
+        </div> <!-- Fim Topo -->
+
+    </div> <!-- FIM Container -->
+
+</body>
+</html>
